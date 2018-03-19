ec1cf76ca5b7fc72d7fceef4196426b73028654b
Joel Corey <joelcorey@fastmail.com>
2018-03-19 09:42:27 -0600

    Updated Fibonacci recursive method and used ternary operator to return

--- 08-fibonacci/script.js
+++ 08-fibonacci/script.js
@@ -4,18 +4,30 @@
 // c. HARDER VERSION: Write a function that does the Fibonacci sequence starting at a number that a user inputs
 // Credit: https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
 
-function fibonacci(num){
-    var a = 1, b = 0, temp;
+//OLD:
+// function fibonacci(num){
+//     var a = 1, b = 0, temp;
   
-    while (num >= 0){
-      temp = a;
-      a = a + b;
-      console.log(a);
-      b = temp;
-      num--;
-    }
-  
-    return b;
-  }
+//     while (num >= 0){
+//       temp = a;
+//       a = a + b;
+//       console.log(a);
+//       b = temp;
+//       num--;
+//     }
+//     return b;
+//   }
+// fibonacci(20);
 
-fibonacci(20);
+//New:
+"use strict";
+
+function fib(n) {
+	//if (n<2) { return n; } else { return fib(n-1)+fib(n-2); }
+	return (n<2 ? n : fib(n-1)+fib(n-2));
+}
+
+
+for (let i = 0; i < 10; i++) {
+	console.log(fib(i));
+}
